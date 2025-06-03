8066f7fbe7468e7a0886b460f50c36db2bc71b80
Apurbo73 <apurbodebnath895@gmail.com>
2025-06-04 00:26:56 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, space;
+    int rows = 4;
+
+    for (i = 1; i <= rows; i++)
+    {
+        // Print spaces
+        for (space = 1; space <= rows - i; space++)
+        {
+            printf(" ");
+        }
+
+        // Print stars
+        for (j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+
+        // Move to next line
+        printf("\n");
+    }
+
+    return 0;
+}
